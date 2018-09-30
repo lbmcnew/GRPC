fbca67968670853d43715aa477e69f80f9a2427f
ltaylor2 <ltwindows@LTWindows.localdomain>
2018-09-30 01:20:25 -0400

    Working two models, need to check alpha/beta for model 2 and do prelim analysis on model 1

--- Scripts/Run_GRPC.R
+++ Scripts/Run_GRPC.R
@@ -3,21 +3,100 @@ setwd("C://Users//Liam//Documents//GRPC")
 
 source("Scripts//Init_GRPC.R", echo=TRUE)
 
-jags.data <- list(N.YEARS = N.YEARS,
-				  LAMBDA.VR = rep(LAMBDA.VR, times=N.YEARS),
-				  COUNTS = COUNTS,
-				  P.SURVEYED = P.SURVEYED)
 
-out.params <- c("a",
-				"b",
-				"sigma.lambda",
-				"lambda.lek")
+Model1 <- function() {
+	jags.data <- list(N.YEARS = N.YEARS,
+					  COUNTS = COUNTS,
+					  P.SURVEYED = P.SURVEYED,
+					  F.SY = F.Y,
+					  F.ASY = F.A,
+					  F.Imm = mean(F.Y, F.A),
+					  S.J = S.J,
+					  S.SY = S.Y,
+					  S.ASY = S.A,
+					  S.Imm = mean(S.Y, S.A))
 
-ipm <- jags(data=jags.data, 
-			parameters.to.save=out.params,
-			model.file="Scripts//ipm.GRPC.jags",
-			n.chains=3,
-			n.thin=5,
-			n.iter=20000,
-			n.burnin=10000,
-			parallel=TRUE)
+	jags.inits <- function() {
+		list(m.numImms=runif(1,0,100),
+			 N.Imm=c(NA,round(runif(N.YEARS-1,0,100))))
+	}
+
+	out.params <- c("N.SY",
+					"N.ASY",
+					"N.Imm",
+					"N.tot",
+					"omega",
+					"lambda")
+
+
+	ipm <- jags(data=jags.data, 
+				inits=jags.inits,
+				parameters.to.save=out.params,
+				model.file="Scripts//ipm_2.GRPC.jags",
+				n.chains=3,
+				n.thin=10,
+				n.burnin=50000,
+				n.iter=100000,
+				parallel=TRUE)
+
+	# summarize ipm output for analyses/visualization
+	out <- as.data.frame(ipm$summary)
+	out <- bind_cols(as.data.frame(rownames(out)), out)
+	colnames(out)[1] <- "param"
+
+	out <- separate(out, param, c("param", "year"), sep="\\[|\\]", 
+	                extra="drop", fill="right")
+
+	colnames(out) <- c("param", "year", "mean", "sd", "low", 
+	                   "twentyfive", "mid", "seventyfive", "high", 
+	                   "rhat", "neff", "overlap", "f")
+
+	out$year <- as.numeric(out$year) + 2006
+
+	ipm_sim <- ipm$sims.list    # the raw results at every iterations
+
+	return(list(out=out, ipm_sim=ipm_sim))
+}
+
+Model2 <- function() {
+	jags.data <- list(N.YEARS = N.YEARS,
+					  LOG.LAMBDA.VR = rep(log(LAMBDA.VR), times=N.YEARS),
+					  COUNTS = COUNTS,
+					  P.SURVEYED = P.SURVEYED)
+
+	out.params <- c("a",
+					"b",
+					"sigma.lambda",
+					"lambda.lek",
+					"theta.vr")
+
+	ipm <- jags(data=jags.data, 
+				parameters.to.save=out.params,
+				model.file="Scripts//ipm_1.GRPC.jags",
+				n.chains=3,
+				n.thin=10,
+				n.burnin=50000,
+				n.iter=100000,
+				parallel=TRUE)
+
+	# summarize ipm output for analyses/visualization
+	out <- as.data.frame(ipm$summary)
+	out <- bind_cols(as.data.frame(rownames(out)), out)
+	colnames(out)[1] <- "param"
+
+	out <- separate(out, param, c("param", "year"), sep="\\[|\\]", 
+	                extra="drop", fill="right")
+
+	colnames(out) <- c("param", "year", "mean", "sd", "low", 
+	                   "twentyfive", "mid", "seventyfive", "high", 
+	                   "rhat", "neff", "overlap", "f")
+
+	out$year <- as.numeric(out$year) + 2006
+
+	ipm_sim <- ipm$sims.list    # the raw results at every iterations
+
+	return(list(out=out, ipm_sim=ipm_sim))
+}
+
+ipm <- Model1()
+out <- ipm$out
